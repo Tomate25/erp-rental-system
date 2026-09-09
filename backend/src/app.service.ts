b199c670c0a81a4d195735ef79c9b110dddb5e72
Tomate25 <anlrocha2006@gmail.com>
2026-09-08 18:11:27 -0600

    feat: respuesta informativa en endpoint raiz api/v1

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      sistema: 'ERP Rental Management System - BM Construcciones',
+      version: '1.0.0',
+      estado: 'online',
+      salud: '/api/v1/health',
+      portalWeb: 'http://localhost:5173',
+      mensaje: 'API REST operativa y lista para conexiones.',
+    };
   }
 }
+
